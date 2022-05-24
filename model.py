73336ab00a2f8dfd9b782c62fb14dd0487cb0dc0
Hung-Hsuan Chen <hhchen@acm.org>
2022-05-24 22:30:01 +0800

    Add main function

--- model.py
+++ model.py
@@ -59,59 +59,59 @@ def save_pr_curve(y_final, y_pred, pr_curve_filename):
     plt.savefig(pr_curve_filename)
 
 
-X_data, y_data = load_train_data()
+def main():
+    X_data, y_data = load_train_data()
+    X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=37)
 
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=37)
+    model_list = [LogisticRegression(max_iter=600),
+                  SVC(), 
+                  RandomForestClassifier(),]
 
-model_list = [LogisticRegression(max_iter=600),
-              SVC(), 
-              RandomForestClassifier(),]
+    train_scores = []
+    val_scores = []
 
-train_scores = []
-val_scores = []
-
-for model in model_list:
-    train, val = get_classifier_cv_score(model, X_train, y_train,'average_precision')
-    train_scores.append(train)
-    val_scores.append(val)
-    
-models_score = sorted(list(zip(val_scores, train_scores, model_list)), reverse=True)
-
-print("-------------------------------------")
-for val, train, model in models_score:
-    print("Model: {} ".format(model.__class__.__name__))
-
-    print("train_score: {:.3f}".format(train)) 
-
-    print("validation_score: {:.3f}".format(val)) 
+    for model in model_list:
+        train, val = get_classifier_cv_score(model, X_train, y_train,'average_precision')
+        train_scores.append(train)
+        val_scores.append(val)
+        
+    models_score = sorted(list(zip(val_scores, train_scores, model_list)), reverse=True)
 
     print("-------------------------------------")
+    for val, train, model in models_score:
+        print("Model: {} ".format(model.__class__.__name__))
+        print("train_score: {:.3f}".format(train)) 
+        print("validation_score: {:.3f}".format(val)) 
+        print("-------------------------------------")
 
-model = RandomForestClassifier()
+    model = RandomForestClassifier()
 
-model_params = {
-    'n_estimators': randint(120, 700),
-    'max_features': randint(1, 7),
-}
+    model_params = {
+        'n_estimators': randint(120, 700),
+        'max_features': randint(1, 7),
+    }
 
-random = RandomizedSearchCV(model, model_params, n_iter=10, cv=5, random_state=1, verbose=2)
-result = random.fit(X_train, y_train)
-# print('Best Score: %s' % result.best_score_)
-# print('Best Hyperparameters: %s' % result.best_params_)
+    random = RandomizedSearchCV(model, model_params, n_iter=10, cv=5, random_state=1, verbose=2)
+    result = random.fit(X_train, y_train)
+    # print('Best Score: %s' % result.best_score_)
+    # print('Best Hyperparameters: %s' % result.best_params_)
 
-pipeline = Pipeline([('preprocessing', StandardScaler()), ('classifier', random.best_estimator_)])
-pipeline.fit(X_train, y_train)
-joblib.dump(pipeline, 'Random_model')
+    pipeline = Pipeline([('preprocessing', StandardScaler()), ('classifier', random.best_estimator_)])
+    pipeline.fit(X_train, y_train)
+    joblib.dump(pipeline, 'Random_model')
 
-X_final, y_final = load_test_data()
-# print("Test score: {:.3f}".format(pipeline.score(X_final, y_final)))
+    X_final, y_final = load_test_data()
+    # print("Test score: {:.3f}".format(pipeline.score(X_final, y_final)))
 
-y_pred = pipeline.predict_proba(X_final)[:,1]
+    y_pred = pipeline.predict_proba(X_final)[:,1]
 
-print("accuracy: %f" % (metrics.accuracy_score(y_final, y_pred > .5)))
-print("precision: %f" % (metrics.precision_score(y_final, y_pred > .5)))
-print("recall: %f" % (metrics.recall_score(y_final, y_pred > .5)))
-print('Confusion matrix:\n', confusion_matrix(y_final, y_pred > .5))
-save_roc_curve(y_final, y_pred, "roc_curve.png")
-save_pr_curve(y_final, y_pred, "pr_curve.png")
+    print("accuracy: %f" % (metrics.accuracy_score(y_final, y_pred > .5)))
+    print("precision: %f" % (metrics.precision_score(y_final, y_pred > .5)))
+    print("recall: %f" % (metrics.recall_score(y_final, y_pred > .5)))
+    print('Confusion matrix:\n', confusion_matrix(y_final, y_pred > .5))
+    save_roc_curve(y_final, y_pred, "roc_curve.png")
+    save_pr_curve(y_final, y_pred, "pr_curve.png")
 
+
+if __name__ == "__main__":
+      main()
